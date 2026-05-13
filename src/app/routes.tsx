229b604eb08873741f0f0ef1857cf56c2ec82acc
ikawaiimaha <kawaiimaha03@gmail.com>
2026-05-13 13:04:00 +0400

    Add routing with Layout and HomePage components

---
+++ src/app/routes.tsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from 'react-router-dom';
+import { Layout } from '../components/layout/Layout';
+import { HomePage } from '../pages/HomePage';
+
+export const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [{ index: true, element: <HomePage /> }],
+  },
+]);
